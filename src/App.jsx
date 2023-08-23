78297de3c71166c95cf04e39898733bc0e1e8af4
YagoGu <yago.gutierrez15@gmail.com>
2023-08-23 16:19:18 +0200

    Iteration 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import FoodBox from "./components/FoodBox";
+import AddFoodForm from "./components/AddFoodForm"
 import foodsJson from "./foods.json"
 import { useState } from 'react';
 
@@ -13,9 +14,15 @@ function App() {
     setFoods(filteredFood)
   }
 
+  const createFood = (newFood) => {
+    const moreFood = [newFood, ... foods]
+    setFoods(moreFood)
+  }
+
   return (
     <div className="App">
       <h1>LAB | React IronNutrition</h1>
+      <AddFoodForm key="id" createFood={createFood}/>
       {foods.map((food) => <FoodBox key={food.id} food={food} deleteFood={deleteFood}/> )}
     </div>
   );
--- src/components/AddFoodForm.jsx
+++ src/components/AddFoodForm.jsx
@@ -1,1 +1,79 @@
 // Your code here
+import { useState } from "react";
+
+function AddFoodForm(props) {
+
+    const [name, setName] = useState("");
+    const [image, setImage] = useState("");
+    const [calories, setCalories] = useState(0);
+    const [servings, setServings] = useState(0);
+
+    function resetInputs(){
+        setName("")
+        setImage("")
+        setCalories(0)
+        setServings(0)
+    }
+
+    const handleSubmit = (e) => {
+        // I don't want the page to reload
+        e.preventDefault();
+        
+        const newFood = {
+            name: name,
+            image: image,
+            calories: calories,
+            servings: servings
+        }
+
+        console.log(newFood);
+        // I want to change the array of movies that it's in the parent component
+        // in the props I have access to the createMovie function
+        props.createFood(newFood);
+        // when we create the new movie we set al the values to the initial ones
+        resetInputs();
+    }
+
+    return (
+        <div>
+            <h3>Add Food Entry</h3>
+            <form>
+                <label>Name: </label>
+                <input
+                type="text"
+                name="name"
+                onChange={(event) => setName(event.target.value)}
+                value={name}
+                />
+
+                <label>Image: </label>
+                <input
+                type="text"
+                name="image"
+                onChange={(event) => setImage(event.target.value)}
+                value={image}
+                />
+
+                <label>Calories: </label>
+                <input
+                type="number"
+                name="calories"
+                onChange={(event) => setCalories(event.target.value)}
+                value={calories}
+                />
+
+                <label>Servings: </label>
+                <input
+                type="number"
+                name="servings"
+                onChange={(event) => setServings(event.target.value)}
+                value={servings}
+                />
+
+                <button onClick={(e) => handleSubmit(e)} type="submit">Add a Food</button>
+            </form>
+        </div>
+    )
+}
+
+export default AddFoodForm;
